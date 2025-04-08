87d4f32d065e391636dd6f86db1d8b82c0106d47
Euangeliov <eva1europa@gmail.com>
2025-04-08 10:18:00 +0900

    design: 컨테이너 내부 아이템영역 높이 일치

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>Profile</div>
-
+  <div class="profile">이름: 이메일:</div>
   <div class="layout">
     <div class="container">
       <p class="title">계정 관리</p>
@@ -46,8 +46,10 @@
   border: 1px solid;
   background-color: rgb(255, 188, 0);
   padding: 2rem;
-  margin: 0rem;
+  margin: 1rem;
   grid-column: span 6;
+  display: flex;
+  flex-direction: column;
 }
 
 .layout .container .title {
@@ -58,6 +60,11 @@
   border: 1px solid;
   background-color: rgb(96, 88, 76);
   padding: 1rem;
+  flex: 1; /* 높이를 유연하게 */
+  display: flex;
+  flex-direction: column;
+  justify-content: center; /* 수직 가운데 정렬 */
+  align-items: flex-start; /* 수평 왼쪽 정렬 */
 }
 
 .layout .container .article button {
