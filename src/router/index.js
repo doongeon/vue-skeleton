ef1b468c3cb063d4733e0e5c2f4b5ecc11d29d4f
07-augst <ek991931@gmail.com>
2025-04-08 14:46:36 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/HomeView.vue'
+import TransactionHistory from '@/components/TransactionHistory.vue'
 import HistoryDetail from '@/views/HistoryDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/history',
+      name: 'history',
+      component: TransactionHistory,
+    },
     {
       path: '/history/:id',
       name: 'detail',
