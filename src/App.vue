aba3699a9c3a84d80e060db4393f0c46c0dfd4f6
Euangeliov <eva1europa@gmail.com>
2025-04-08 16:40:32 +0900

    feat: QuickAdd 기본화면에 배치

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from './views/Header.vue'
+import QuickAdd from './views/QuickAdd.vue'
 </script>
 
 <template>
   <div>
     <Header />
     <Router-View />
+    <QuickAdd />
   </div>
 </template>
