1b88ed58efde193bf83b30ebc59a5c7d768c48cb
SeYeon <syp9908@gmail.com>
2025-04-09 07:46:22 +0900

    홈 컴포넌트 #33
    홈 컴포넌트

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 차트
+import VueApexCharts from 'vue3-apexcharts'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+//
+app.component('ApexChart', VueApexCharts)
 
 app.mount('#app')
