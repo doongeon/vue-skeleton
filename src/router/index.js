a4f630225f78a15201473777ca7545f70b01d364
suyeon-0203 <hsy05761@naver.com>
2025-04-08 11:49:29 +0900

    feature/header

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Header from '@/views/Header.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/header',
+      name: 'header',
+      component: Header,
     },
   ],
 })
