a89c3f595e98ac0fd00328b722b320251048a496
donggeon Na <ndmb2012@gmail.com>
2025-04-08 20:57:45 +0900

    feat: 전체 컴포넌트 layout 작업

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Header from '../views/Header.vue'
-import TransactionHistory from '@/components/TransactionHistory.vue'
 import AddTransaction from '@/views/AddTransaction.vue'
-import TransactionHistory from '@/components/TransactionHistory.vue'
-import EditTransaction from '@/views/EditTransaction.vue'
 import TransactionHistory from '@/views/TransactionHistory.vue'
-
+import EditTransaction from '@/views/EditTransaction.vue'
+import HistoryDetail from '@/views/HistoryDetail.vue'
+import StatisticView from '@/views/StatisticView.vue'
+import Layout from '@/views/Layout.vue'
+import ProfileView from '@/views/ProfileView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/header',
-      name: 'header',
-      component: Header,
-    },
-    {
-      path: '/history/add',
-      name: 'add',
-      component: AddTransaction,
-    },
-    {
-      path: '/history/:id/edit',
-      name: 'edit',
-      component: EditTransaction,
+      component: Layout,
+      children: [
+        { path: '', name: 'home', component: HomeView },
+        { path: 'history', name: 'history', component: TransactionHistory },
+        { path: 'history/add', name: 'addHistory', component: AddTransaction },
+        {
+          path: 'history/:id',
+          name: 'historyDetail',
+          component: HistoryDetail,
+        },
+        {
+          path: 'history/:id/edit',
+          name: 'editHistory',
+          component: EditTransaction,
+        },
+        { path: 'analysis', name: 'analysis', component: StatisticView },
+        { path: 'calendar', name: 'calendar', component: HomeView },
+        { path: 'profile', name: 'profile', component: ProfileView },
+        {
+          path: '/:pathMatch(.*)*',
+          name: 'notFound',
+          component: NotFoundView,
+        },
+      ],
     },
   ],
 })
