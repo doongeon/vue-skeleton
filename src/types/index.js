466ed25c9be721c747cc6c9229327b561ca9e160
donggeon Na <ndmb2012@gmail.com>
2025-04-08 09:41:18 +0900

    feat: transaction 피니아 스토어 구현

---
+++ src/types/index.js
@@ -0,0 +1,18 @@
+export const TRANSACTION_TYPE = {
+  expense: '1',
+  income: '2',
+}
+
+export const TRANSACTION_CATEGORY = {
+  식비: '1',
+  교통: '2',
+  '문화/여가': '3',
+  '술/유흥': '4',
+  쇼핑: '5',
+  '여행/숙박': '6',
+  월급: '7',
+  용돈: '8',
+  보너스: '9',
+  매매: '10',
+  이자: '11',
+}
