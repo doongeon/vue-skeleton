df1cf11bb22b8cab7e3a5eabfc94520163a59e9b
07-augst <ek991931@gmail.com>
2025-04-08 11:16:12 +0900

    fix: index, feat: views에 page 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/HomeView.vue'
 import TransactionHistory from '@/components/TransactionHistory.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
       path: '/history',
-      name: 'TransactionHistory',
+      name: 'history',
       component: TransactionHistory,
     },
   ],
---
+++ src/views/HistoryPage.vue
@@ -0,0 +1,8 @@
+<script setup>
+import TransactionHistory from '@/components/TransactionHistory.vue'
+</script>
+
+<template>
+  <h1>거래내역</h1>
+  <TransactionHistory />
+</template>
