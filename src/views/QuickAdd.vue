11855f63779068f9deb8a2b6eff3673494e2b778
Euangeliov <eva1europa@gmail.com>
2025-04-08 11:17:50 +0900

    feat: 빠른 추가 버튼 design: 버튼 디자인

---
+++ src/views/QuickAdd.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <button class="icon">빠른 추가</button>
+</template>
+
+<style scoped>
+.icon {
+  border: none;
+  font-size: 1rem;
+  color: white;
+  border-radius: 0.5rem;
+  background-color: rgb(255, 188, 0);
+  display: inline-block;
+  padding: 1rem 1.5rem;
+}
+</style>
