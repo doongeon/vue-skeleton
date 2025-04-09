9b2f9ea210f2b942bc2aa2724add06acd4dcce48
donggeon Na <ndmb2012@gmail.com>
2025-04-09 11:47:42 +0900

    fix: 빌드가 안되는 에러코드 수정

--- src/main.js
+++ src/main.js
@@ -14,6 +14,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(VCalendar, { componentPrefix: 'V',})
+app.use(VCalendar, { componentPrefix: 'V' })
+app.use(VueApexCharts)
+
+app.component('ApexChart', VueApexCharts)
 
 app.mount('#app')
