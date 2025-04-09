3231a5c7ae422ebd94ebba28da06c2fd5a58c76e
donggeon Na <ndmb2012@gmail.com>
2025-04-09 10:26:57 +0900

    fix: 빌드 오류 코드 삭제

--- src/main.js
+++ src/main.js
@@ -7,9 +7,6 @@ import VueApexCharts from 'vue3-apexcharts'
 import App from './App.vue'
 import router from './router'
 
-// 차트
-import VueApexCharts from 'vue3-apexcharts'
-
 const app = createApp(App)
 
 app.use(createPinia())
