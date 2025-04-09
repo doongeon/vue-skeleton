df9fc1bc291fcce571fd55ab5ff4e2e1184a3723
donggeon Na <ndmb2012@gmail.com>
2025-04-09 10:26:35 +0900

    refactor: computed 변수로 변경

--- src/components/AnalysisList.vue
+++ src/components/AnalysisList.vue
@@ -6,9 +6,17 @@ const props = defineProps({
   categorialTransaction: Object,
 })
 
-const sum = computed(() =>
-  Object.values(props.categorialTransaction).reduce((sum, amount) => sum + amount, 0),
-)
+const categorialTransactions = computed(() => {
+  return Object.keys(props.categorialTransaction)
+    .map((key) => {
+      return { id: key, amount: props.categorialTransaction[key] }
+    })
+    .sort((a, b) => b.amount - a.amount)
+})
+
+const sum = computed(() => {
+  return categorialTransactions.value.reduce((sum, e) => sum + e.amount, 0)
+})
 
 const getCategoryName = (categoryId) => {
   return Object.keys(TRANSACTION_CATEGORY).find(
@@ -16,8 +24,8 @@ const getCategoryName = (categoryId) => {
   )
 }
 
-const getRatio = (categoryId) => {
-  return Math.round((props.categorialTransaction[categoryId] / sum.value) * 1000) / 10
+const getRatio = (amount) => {
+  return Math.round((amount / sum.value) * 1000) / 10
 }
 </script>
 
@@ -25,25 +33,25 @@ const getRatio = (categoryId) => {
   <ul class="card p-3 list-group list-group-flush">
     <li
       class="list-group-item d-flex align-items-center justify-content-between"
-      v-for="categoryId in Object.keys(props.categorialTransaction)"
-      :key="categoryId"
+      v-for="categorialTransaction in categorialTransactions"
+      :key="categorialTransaction.id"
     >
-      <span style="width: 70px">{{ getCategoryName(categoryId) }}</span>
+      <span style="width: 70px">{{ getCategoryName(categorialTransaction.id) }}</span>
       <div
         class="progress w-50"
         role="progressbar"
         aria-label="Success example"
-        :aria-valuenow="getRatio(categoryId)"
+        :aria-valuenow="getRatio(categorialTransaction.amount)"
         aria-valuemin="0"
         aria-valuemax="100"
       >
         <div
-          class="progress-bar text-bg-success"
-          :style="{ width: getRatio(categoryId) + '%' }"
+          class="progress-bar bg-warning"
+          :style="{ width: getRatio(categorialTransaction.amount) + '%' }"
         ></div>
       </div>
       <span class="me-0" style="width: 90px">{{
-        props.categorialTransaction[categoryId].toLocaleString()
+        categorialTransaction.amount.toLocaleString()
       }}</span>
     </li>
   </ul>
