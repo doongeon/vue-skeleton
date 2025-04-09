e2d76925d32b7eca700c76c092bf791a0361b511
07-augst <ek991931@gmail.com>
2025-04-09 11:30:35 +0900

    style: filter, historylist, transaction 수정

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueApexCharts from 'vue3-apexcharts'
 
 import App from './App.vue'
 import router from './router'
