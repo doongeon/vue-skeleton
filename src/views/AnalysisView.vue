e04df96f2bca2d06cc4c3c10d64e4cb4b39801b6
donggeon Na <ndmb2012@gmail.com>
2025-04-10 09:07:16 +0900

    fix: 카테고리를 피니아 스토어에서 받아오도록 수정

--- src/views/AnalysisView.vue
+++ src/views/AnalysisView.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { useTransactionStore } from '@/stores/transactionStore'
-import { TRANSACTION_TYPE, TRANSACTION_CATEGORY } from '@/types'
+import { useTransactionCategoryStore } from '@/stores/transactionCategoryStore'
+import { TRANSACTION_TYPE } from '@/types'
 import { computed, reactive } from 'vue'
 import PieChart from '@/components/PieChart.vue'
 import AnalysisList from '@/components/AnalysisList.vue'
 
 const transactionStore = useTransactionStore()
+const transactionCategoryStore = useTransactionCategoryStore()
 const transactions = computed(() => transactionStore.states.transactions)
+const transactionCategories = computed(() => transactionCategoryStore.states.transactionCategories)
 
 const states = reactive({
   period: 1,
@@ -69,9 +72,13 @@ const totalExpense = computed(() => {
 })
 
 const getCategoryName = (categoryId) => {
-  return Object.keys(TRANSACTION_CATEGORY).find(
-    (categoryName) => TRANSACTION_CATEGORY[categoryName] === categoryId,
-  )
+  if (transactionCategories.value.length === 0) {
+    return 'loading'
+  }
+
+  return transactionCategories.value.find(
+    (transactionCategory) => transactionCategory.id === categoryId,
+  ).name
 }
 </script>
 
